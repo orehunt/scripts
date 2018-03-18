bc005695d17ce36780b535f093ddae2b3ddf9907
untoreh <bassobassista@gmail.com>
2018-03-18 18:22:26 +0100

    added build script (no libs) , support for aesni lacking hosts
    Former-commit-id: debcf90917e981b9dd160a89d5d017598867f1da

--- build.sh
+++ build.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
 fork=https://github.com/bobbieltd/xmr-node-proxy
+bpath=node_modules/multi-hashing/build/Release
 export DEBIAN_FRONTEND=noninteractive
 name=xnp
 appname=server.js
 
 apt update && apt upgrade -y
 apt -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev
-rm -rf $(basename $fork)
-git clone $fork
+rm -rf $name
+git clone $fork $name
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v6.9.2
 
 ## rename folder and main bin for pkg children procs
-mv $(basename $fork) $name
 cd $name
 npm install
-npm install cryptonote-util multi-hashing
 npm install -g pkg
 mv proxy.js $appname
-patch package.json package.json.patch
-patch $name proxy.js.patch
-pkg -t node6-linux-x64 package.json
+patch package.json ../package.json.patch
+patch $appname ../proxy.js.patch
+pkg -t node6-linux-x64 package.json -o pkgbin
+rm -rf bindings/ && mkdir bindings && \
+    find node_modules/{bignum,cryptonote-util,multi-hashing} \
+         -name \*.node ! -path \*obj.target\* | \
+        xargs -I{} cp --parents -a {} bindings/
+patch package.json ../package.json_nan.patch
+npm install multi-hashing
+cp ${bpath}/*.node bindings/${bpath}/multihashing_nan.node
+mv pkgbin ../
+rm -rf ../bindings
+mv bindings ../
+cd ../ && rm -rf $name && mv pkgbin $name
