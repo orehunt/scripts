2f4761f46b03b4265c88115779c41d7222e06df3
untoreh <bassobassista@gmail.com>
2018-04-08 06:38:53 +0200

    added obfs script

---
+++ deploy/obfs.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+[ -z "$1" ] &&
+	echo "provide a file to obfuscate." && 
+	exit 1
+
+tool=bash-obfuscate
+outf="${1}.obfs"
+
+[ -f /tmp/.obfs-installed ] || npm list -g "$tool" || { npm install -g "$tool" && touch /tmp/.obfs-installed }
+echo 'set +x' > "$outf"
+$tool -r "$1" | sed 's/eval/exec bash <<< /' >> "$outf" &&
+	echo "file $outf is obfuscated"
