d2f00343978b7e9e2c384aaafd83798ff910243c
untoreh <bassobassista@gmail.com>
2018-04-11 20:28:09 +0200

    added proot, to override old kernel glib limitations (ovz)
    Former-commit-id: b35e57f055a38bd941da23f555c1db37bcd0685d

--- start.sh
+++ start.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 AES_AVL=$(cat /proc/cpuinfo | grep -o -m1 aes)
-LIBC=${LIBC:-musl}
-# LIBC=${LIBC:-glib}
+KRN=$(uname -r)
 DIR=$(dirname $(realpath "$0"))
 PATH="$DIR:$PATH"
+proot=wrap
+
 cd $DIR
 if [ -z "$AES_AVL" ]; then
     mvpath="node_modules/multi-hashing/build/Release"
     mv ${mvpath}/multihashing_nan.node \
        ${mvpath}/multihashing.node
 fi
-LD_LIBRARY_PATH=$DIR/lib/$LIBC:/usr/lib:/lib:/usr/lib64:/lib64 \
-               exec xnp
+
+if [ "$KRN" != "${KRN/2.6}" ]; then ## hide kernel if too old
+    LD_LIBRARY_PATH=$DIR/lib \
+                   PATH=$DIR/lib:$PATH \
+               exec $proot -k 4.14 xnp 2>/dev/null
+else
+    LD_LIBRARY_PATH=$DIR/lib \
+                   PATH=$DIR/lib:$PATH \
+                   exec xnp 2>/dev/null
+fi
