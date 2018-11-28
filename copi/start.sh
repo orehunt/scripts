dfa361947c1c997e230e1920a7b37bec2cba2318
untoreh <bassobassista@gmail.com>
2018-11-28 15:29:34 +0100

    added sleep backup added haproxy config runtime path reverted copi to drop shell

--- copi/start.sh
+++ copi/start.sh
@@ -2,12 +2,12 @@
 
 DIR=$(dirname $(realpath "$0"))
 export PATH="$DIR:$PATH"
-type copi || { echo "containerpilot not found, aborting."; exit 1; }
-type hpr || { echo "haproxy not found, aborting."; exit 1; }
+type copi || { echo "containerpilot not found, aborting."; sleep 3; exit 1; }
+type hpr || { echo "haproxy not found, aborting."; sleep 3; exit 1; }
+# getent passwd haproxy ||  adduser -D haproxy || { echo "haproxy user not found and couldn't be added"; sleep 3; exit 1; }
 
 copi_config=copi.json5
 hpr_config=hpr.cfg
 copi -config $copi_config -template -out run.$copi_config
-copi -config $hpr_config -template -out $hpr_config
-# exec copi -config run.$copi_config
-copi -config run.$copi_config
+copi -config $hpr_config -template -out run.$hpr_config
+exec copi -config run.$copi_config
