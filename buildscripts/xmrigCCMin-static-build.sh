bb9192f162bc3c1da6212ae46e9b02da0d097ab9
untoreh <bassobassista@gmail.com>
2018-12-21 10:35:41 +0100

    mac os support

--- buildscripts/xmrigCCMin-static-build.sh
+++ buildscripts/xmrigCCMin-static-build.sh
@@ -3,6 +3,11 @@ set -e
 
 prevpath=$PWD
 
+if [ "$1" = mac ]; then
+    brew install bash gnu-sed gpatch gcc cmake libuv openssl libmicrohttpd boost
+    PATH=/usr/local//Cellar/gnu-sed/4.6/libexec/gnubin/:$PATH
+fi
+
 ## distro pkgs
 if type apk; then
     apk --allow-untrusted del -f openssl-dev
@@ -12,7 +17,11 @@ fi
 ## get void ; chroot void ...
 prevpath=${PWD}
 cd ~/
-jobs=$(nproc || cat /proc/cpuinfo | grep -i "cpu cores" | wc -l)
+if [ "$1" = mac ]; then
+    jobs=$(sysctl -n hw.physicalcpu)
+else
+    jobs=$(nproc || cat /proc/cpuinfo | grep -i "cpu cores" | wc -l)
+fi
 [ -z "$jobs" ] && jobs=1
 
 rm -rf xmrigCC
@@ -33,25 +42,40 @@ sed 's/m_daemonized(false)/m_daemonized(true)/' -i ../src/Options.cpp
 sed -r 's/(kDonateLevel = )([0-9]+)/\10/' -i ../src/donate.h
 
 ## build
-if [ -z "$(ldd $(which gcc) | grep -i musl)" ]; then
-    export CC=${CC:-"$(uname -m)-linux-musl-gcc"}
+if [ "$1" != mac ]; then
+    if [ -z "$(ldd $(which gcc) | grep -i musl)" ]; then
+        export CC=${CC:-"$(uname -m)-linux-musl-gcc"}
+    fi
 fi
-export MAKEFLAGS=" -j $(nproc) "
-export CFLAGS=" -Ofast -Flto"
+export MAKEFLAGS=" -j $jobs "
+if [ "$1" = mac ]; then
+    export CFLAGS=" -Ofast"
+else
+    export CFLAGS=" -Ofast -Flto"
+fi
 export CPPFLAGS="$CFLAGS" CXXFLAGS="$CFLAGS" LDFLAGS="$CFLAGS"
 ## set build flags
-sed '/add_executable/iset(CMAKE_EXE_LINKER_FLAGS " -static '"$CFLAGS"'")' -i ../CMakeLists.txt
-sed -r '/target_link_libraries\(xmrigMiner xmrig_common/{n;s#(.*?)\)#\1 /usr/lib/libstdc++.a /usr/lib/libc.a )#}' -i ../CMakeLists.txt
+if [ "$1" != mac ]; then
+    sed '/add_executable/iset(CMAKE_EXE_LINKER_FLAGS " -static '"$CFLAGS"'")' -i ../CMakeLists.txt
+    sed -r '/target_link_libraries\(xmrigMiner xmrig_common/{n;s#(.*?)\)#\1 /usr/lib/libstdc++.a /usr/lib/libc.a )#}' -i ../CMakeLists.txt
+fi
 
-cmake .. \
--DWITH_CC_SERVER=OFF -DWITH_HTTPD=OFF \
--DUV_LIBRARY=/usr/lib/libuv.a \
--DOPENSSL_SSL_LIBRARY=/usr/lib/libssl.a \
--DOPENSSL_CRYPTO_LIBRARY=/usr/lib/libcrypto.a
+if [ "$1" != mac ]; then
+    cmake .. \
+          -DWITH_CC_SERVER=OFF -DWITH_HTTPD=OFF \
+          -DUV_LIBRARY=/usr/lib/libuv.a \
+          -DOPENSSL_SSL_LIBRARY=/usr/lib/libssl.a \
+          -DOPENSSL_CRYPTO_LIBRARY=/usr/lib/libcrypto.a
+else
+    cmake .. -DUV_LIBRARY=/usr/local/lib/libuv.a \
+          -DOPENSSL_SSL_LIBRARY=/usr/local/opt/openssl/lib/libssl.a \
+          -DOPENSSL_CRYPTO_LIBRARY=/usr/local/opt/openssl/lib/libcrypto.a \
+          -DBOOST_ROOT=/usr/local/lib \
+          -DWITH_CC_SERVER=OFF \
+          -DWITH_HTTPD=OFF \
+          -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl \
+          -DWITH_ASM=OFF
+fi
 
 make -j $jobs xmrigMiner
 mv xmrigMiner $prevpath/xmrig
-
-## MACOS
-# brew install bash gnu-sed gpatch gcc cmake libuv openssl libmicrohttpd boost
-# cmake .. -DUV_LIBRARY=/usr/local/lib/libuv.a -DOPENSSL_SSL_LIBRARY=/usr/local/opt/openssl/lib/libssl.a -DOPENSSL_CRYPTO_LIBRARY=/usr/local/opt/openssl/lib/libcrypto.a -DBOOST_ROOT=/usr/local/lib -DWITH_CC_SERVER=ON -DWITH_HTTPD=OFF -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DWITH_ASM=OFF
