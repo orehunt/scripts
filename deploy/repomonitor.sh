0d265bad106695dbc7db41f638ec59e0e77b4926
untoreh <bassobassista@gmail.com>
2019-01-08 15:15:47 +0100

    buildscripts cleanup added repo monitor script

---
+++ deploy/repomonitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. /etc/profile.d/func.sh || { echo missing required functions file /etc/profile.d/func.sh; exit 1; }
+interval=${MONITORED_INTERVAL:-3600}
+repo=${MONITORED_REPO}
+[ -z "$repo" ] && { echo missing \$MONITORED_REPO; exit 1; }
+ref=${HOOK_REF}
+[ -z "$ref" ] && { echo missing \$HOOK_REF; exit 1; }
+token=${HOOK_TOKEN}
+[ -z "$token" ] && { echo missing \$HOOK_TOKEN; exit 1; }
+url=${HOOK_URL}
+[ -z "$url" ] && { echo missing \$HOOK_URL; exit 1; }
+
+lv=$(</var/log/repomonitor_lv.log)
+while :; do
+    nlv=$(last_version $repo)
+    if [ "$nlv" != "$lv" ]; then
+        wget -qO- --post-data="token=${HOOK_TOKEN}&ref=""${HOOK_REF}" "${HOOK_URL}"
+        lv=$nlv
+        echo "$lv" > /var/log/repomonitor_lv.log
+    fi
+done
