3c87c98b522ec12234f603fda69b8899bb4fa476
untoreh <bassobassista@gmail.com>
2019-02-02 18:46:30 +0100

    added mm build script

---
+++ build_mm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+nodeV=8
+fork=https://github.com/MoneroOcean/meta-miner
+export DEBIAN_FRONTEND=noninteractive
+
+name=mm
+apt update && apt upgrade -y
+apt -y install curl
+rm -rf $name
+
+git clone $fork $name
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v$nodeV
+
+cp ./mm_package.json $name/
+cd $name
+npm install -g pkg
+pkg -t node$nodeV-linux-x64 package.json -o pkgbin
+mv pkgbin ../
+cd ../ && rm -rf $name && mv pkgbin $name
