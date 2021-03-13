33a0e18edda38af013a68106b51ab2a5159a6c99
orehunt <fra@localhost>
2021-03-13 10:18:19 +0100

    removed pkg

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ bpath=node_modules/multi-hashing/build/Release
 # modules="{bignum,cryptoforknote-util,cryptonight-hashing,multi-hashing,cryptonote-util,semipool-ipbc-util}"
 export DEBIAN_FRONTEND=noninteractive
 name=xnp
-appname=server.js
+appname=proxy.js
 
 apt update && apt upgrade -y
 apt -y install git jq python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev libzmq3-dev libsodium-dev
@@ -26,25 +26,25 @@ cd $name
 export JOBS=max
 npm install --ignore-scripts
 
-find ./ -name binding.gyp | xargs sed 's/-march=native/-march=athlon64 -maes /' -i
+find ./ -name binding.gyp | xargs sed 's/-march=native/-march=athlon64 -maes/' -i
 find ./ -name binding.gyp | xargs sed '/cn_gpu_/d' -i
 npm install
 npm rebuild
-npm install -g pkg@4.2.5
-mv proxy.js $appname
+# npm install -g pkg@4.2.5
+# mv proxy.js $appname
 # jq '.+ {"bin": "server.js",
 #   "pkg": {
 #          "scripts": "lib/**/*.js"
 #    }}' <package.json >package.json.new && mv package.json.new package.json
 # patch $appname ../proxy.js.patch
-# sed 's/sendReply(miner.error)/if (miner.error != "Unauthorized access" ) sendReply(miner.error)/' \
-# -i $appname
-pkg -t node$nodeV-linux-x64 package.json -o pkgbin
-rm -rf bindings/ && mkdir bindings &&
-	find node_modules \
-		-regextype sed -regex ".*\(Release\|binding\)/[^\/]*.node" |
-	xargs -I{} cp --parents -a {} bindings/
-mv pkgbin ../
-rm -rf ../bindings
-mv bindings ../
-cd ../ && rm -rf $name && mv pkgbin $name
+sed 's/sendReply(miner.error)/if (miner.error != "Unauthorized access" ) sendReply(miner.error)/' -i $appname
+# pkg -t node$nodeV-linux-x64 package.json -o pkgbin
+# rm -rf bindings/ && mkdir bindings &&
+# 	find node_modules \
+# 		-regextype sed -regex ".*\(Release\|binding\)/[^\/]*.node" |
+# 	xargs -I{} cp --parents -a {} bindings/
+# mv pkgbin ../
+# rm -rf ../bindings
+# mv bindings ../
+# cd ../ && rm -rf $name && mv pkgbin $name
+cp ../config.json ../accessControl.jsson $name/
--- fix_links.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ln -srf ./bindings/node_modules ./node_modules
-ln -srf node_modules/bignum/build/Release/ node_modules/bignum/binding
